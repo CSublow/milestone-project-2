c9ec9102fad56d0cc64fceb82994af0ea8b8181a
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-11-07 16:16:39 +0000

    Formatted numberWithCommas function

--- assets/js/numberFormatter.js
+++ assets/js/numberFormatter.js
@@ -2,7 +2,6 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
 }
 
-
     $('.points').each(function(){
     var v_pound = $(this).html();
     v_pound = numberWithCommas(v_pound);
