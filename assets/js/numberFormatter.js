c18a9280361b00cd984792ea96fb93bb114d7da6
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-11-07 16:20:06 +0000

    Updated numberWithCommas function to reflect markup for this project

--- assets/js/numberFormatter.js
+++ assets/js/numberFormatter.js
@@ -2,11 +2,11 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
 }
 
-    $('.points').each(function(){
-    var v_pound = $(this).html();
-    v_pound = numberWithCommas(v_pound);
+    $('.number-display').each(function(){
+    var numberToFormat = $(this).html();
+    numberToFormat = numberWithCommas(numberToFormat);
 
-    $(this).html(v_pound)
+    $(this).html(numberToFormat)
         
         })
 
