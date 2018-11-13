dd029204deb82fca8a40f4aa0aae626aa7b8e2bb
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-11-13 15:50:19 +0000

    Deleted redundant print_filter code

--- assets/js/chartScript.js
+++ assets/js/chartScript.js
@@ -14,15 +14,6 @@ console.log(w);
 function makeGraph(error, ggData) {
     if (error) throw error;
     
-    //print_filter function, used to see the values for crossfilter objects
-    // function print_filter(filter) {
-    // var f=eval(filter);
-    // if (typeof(f.length) != "undefined") {}else{}
-    // if (typeof(f.top) != "undefined") {f=f.top(Infinity);}else{}
-    // if (typeof(f.dimension) != "undefined") {f=f.dimension(function(d) { return "";}).top(Infinity);}else{}
-    // console.log(filter+"("+f.length+") = "+JSON.stringify(f).replace("[","[\n\t").replace(/}\,/g,"},\n\t").replace("]","\n]"));
-    // }
-    
     var ndx = crossfilter(ggData); //load the data into a crossfilter
     
     //Dimensions
