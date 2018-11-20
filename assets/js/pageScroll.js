abe6bac55373c53f60fa350af55c872b6e29da9d
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-11-20 13:08:47 +0000

    Set all page scroll resets to false

---
+++ assets/js/pageScroll.js
@@ -0,0 +1,21 @@
+//Courtesy of https://scrollrevealjs.org/guide/hello-world.html
+ScrollReveal().reveal('.reveal-one', {
+    delay: 500,
+    duration: 1000,
+    reset: false
+});
+ScrollReveal().reveal('.reveal-two', {
+    delay: 750,
+    duration: 1000,
+    reset: false
+});
+ScrollReveal().reveal('.reveal-three', {
+    delay: 1125,
+    duration: 1000,
+    reset: false
+});
+ScrollReveal().reveal('.reveal-four', {
+    delay: 1500,
+    duration: 1000,
+    reset: false
+});
