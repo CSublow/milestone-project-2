9e466a4221af5df5585998d39e1f75db1bceb5e6
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-11-23 15:59:41 +0000

    Forced page to scroll to top on reload

--- assets/js/pageScroll.js
+++ assets/js/pageScroll.js
@@ -1,4 +1,9 @@
-//Courtesy of https://scrollrevealjs.org/guide/hello-world.html
+//Ensure page refresh forces the page to be scrolled to the top so that the below ScrollReveal() functions work as intended
+window.onbeforeunload = function() {
+    window.scrollTo(0,0);
+}
+
+//Scroll Reveal functionality, courtesy of https://scrollrevealjs.org/guide/hello-world.html
 ScrollReveal().reveal('.reveal-one', {
     delay: 500,
     duration: 1000,
@@ -18,4 +23,4 @@ ScrollReveal().reveal('.reveal-four', {
     delay: 1125,
     duration: 1000,
     reset: false
-});
+}); 
