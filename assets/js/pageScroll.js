780f695b0c1f12d848ecf2180cfe31911ecf264f
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-11-27 20:23:56 +0000

    Did some footer styling and added functionality to Back to Top link

--- assets/js/pageScroll.js
+++ assets/js/pageScroll.js
@@ -24,3 +24,9 @@ ScrollReveal().reveal('.reveal-four', {
     duration: 1000,
     reset: false
 }); 
+
+//The Back to Top function which is invoked when the user clicks on the relevant link in the footer
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
