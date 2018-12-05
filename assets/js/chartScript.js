254125c3c2f4540cf42dbb7cfa244e09c2a5d590
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-12-05 20:41:34 +0000

    Adjusted size of loading text

--- assets/js/chartScript.js
+++ assets/js/chartScript.js
@@ -635,9 +635,9 @@ function makeGraph(error, ggData) {
             });
         };
     //Remove the loading overlay when the document is ready. A delay is set to ensure that users on quick connections, or those who have already visited the site and have cookies saved, can see the screen is a loading screen and not get confused if it disappears quickly
-        $('body').delay(1000).queue(function() { //The loading screen will appear for at least one second
-            $(this).addClass('loaded');
-        });
+        // $('body').delay(1000).queue(function() { //The loading screen will appear for at least one second
+        //     $(this).addClass('loaded');
+        // });
     });
 };
 
