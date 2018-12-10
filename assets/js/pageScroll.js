55d56a8cb7515618ecc342fe999214ef787129e3
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-12-10 14:39:33 +0000

    Added scroll effect to pie chart pop up, removed redundant scroll functions

--- assets/js/pageScroll.js
+++ assets/js/pageScroll.js
@@ -1,29 +1,15 @@
 //Ensure page refresh forces the page to be scrolled to the top so that the below ScrollReveal() functions work as intended
-window.onbeforeunload = function() {
-    window.scrollTo(0,0);
-}
+// window.onbeforeunload = function() {
+//     window.scrollTo(0,0);
+// }
 
-//Scroll Reveal functionality, courtesy of https://scrollrevealjs.org/guide/hello-world.html
-ScrollReveal().reveal('.reveal-one', {
-    delay: 500,
+//Scroll Reveal functionality for the pie chart popup, courtesy of https://scrollrevealjs.org/guide/hello-world.html
+ScrollReveal().reveal('.reveal', {
+    delay: 1500,
     duration: 1000,
-    reset: false
+    reset: true,
+    mobile: true
 });
-ScrollReveal().reveal('.reveal-two', {
-    delay: 600,
-    duration: 1000,
-    reset: false
-});
-ScrollReveal().reveal('.reveal-three', {
-    delay: 700,
-    duration: 1000,
-    reset: false
-});
-ScrollReveal().reveal('.reveal-four', {
-    delay: 700,
-    duration: 1000,
-    reset: false
-}); 
 
 //The Back to Top function which is invoked when the user clicks on the relevant link in the footer
 function topFunction() {
