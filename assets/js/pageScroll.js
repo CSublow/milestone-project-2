f344c637a896d9f1a174fa3ec67ee967e57efe22
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-12-10 17:37:12 +0000

    Removed scrollReveal functionality and deleted all references to it

--- assets/js/pageScroll.js
+++ assets/js/pageScroll.js
@@ -1,16 +1,3 @@
-//Ensure page refresh forces the page to be scrolled to the top so that the below ScrollReveal() functions work as intended
-// window.onbeforeunload = function() {
-//     window.scrollTo(0,0);
-// }
-
-//Scroll Reveal functionality for the pie chart popup, courtesy of https://scrollrevealjs.org/guide/hello-world.html
-ScrollReveal().reveal('.reveal', {
-    delay: 1500,
-    duration: 1000,
-    reset: true,
-    mobile: true
-});
-
 //The Back to Top function which is invoked when the user clicks on the relevant link in the footer
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
