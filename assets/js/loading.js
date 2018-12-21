8d59655074f3353578a85d412ac2064870a48cef
Chris Hengmith <crhengmith@hotmail.co.uk>
2018-12-21 16:17:47 +0000

    Created loading.js, moved loader related js into loading.js. Moved jquery and loading.js into index.html head

---
+++ assets/js/loading.js
@@ -0,0 +1,6 @@
+$( window ).on("load", function() {
+    //Remove the loading overlay when the document is loaded. A delay is set to ensure that users on quick connections, or those who have already visited the site and have cookies saved, can see the screen is a loading screen and not get confused if it disappears quickly
+    $('body').delay(1000).queue(function() { //The loading screen will appear for at least one second
+        $(this).addClass('loaded');
+    });   
+})
